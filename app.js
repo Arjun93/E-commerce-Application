0d8cd4bc3014631c50686626d004d6eb516f4499
Arjun93 <nujra.an@gmail.com>
2015-09-14 02:39:54 -0400

    REST service for registration feature added.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var logout = require('./routes/logout');
 var login = require('./routes/login');
+var register = require('./routes/register');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/login', login);
 app.use('/logout', logout);
+app.use('/registerUser', register);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/register.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  //host     : 'quizapp.ccwtwgtut47e.us-east-1.rds.amazonaws.com',
+  //port : '3306',
+  user     : 'root',
+  password : '12312312',
+  database :'ecommerce',
+});
+
+router.post('/', function(req, res, next) {
+  console.log("Inside post!!");
+  var firstName = req.query.fName;
+  var lastName = req.query.lName;
+  var userAddress = req.query.address;
+  var userCity = req.query.city;
+  var userState = req.query.state;
+  var userZip = req.query.zip;
+  var userEmail = req.query.email;
+  var userName = req.query.uName;
+  var passWord = req.query.pWord;
+  console.log("Before insertion!");
+  var validationResult = validateUserInformation(firstName,lastName,userAddress,userCity,userState,userZip,userEmail,userName,passWord);
+  if(validationResult == true) {
+  	insertUserInformation(firstName,lastName,userAddress,userCity,userState,userZip,userEmail,userName,passWord);
+  	res.json({"message":"Your account has been registered"});
+  }
+  else {
+  	res.json({"message":"there was a problem with your registration"});
+  }
+});
+
+function validateUserInformation(firstName,lastName,userAddress,userCity,userState,userZip,userEmail,userName,passWord) {
+	var validationFlag = 0;
+	if (firstName == "" || lastName =="" || userAddress =="" || userCity =="" ||userState == "" || userZip == "" || userEmail == "" || userName == "" || passWord == "" ) {
+		validationFlag = 1;
+	}
+	if (userState.length != 2 || userZip.length != 5) {
+		validationFlag = 1;
+	}
+	if (validationFlag == 1) {
+		return false;
+	}
+	else {
+		return true;
+	}
+
+}
+
+
+function insertUserInformation(firstName,lastName,userAddress,userCity,userState,userZip,userEmail,userName,passWord) {
+	console.log("inside Insert!!");
+	var insertData = {
+	    firstname: firstName,
+		lastname: lastName,
+		address: userAddress,
+		city: userCity,
+		state: userState,
+		zip: userZip,
+		email: userEmail,
+		username: userName,
+		password: passWord,
+		role: "user",    
+  	};
+	connection.query('INSERT INTO user_credentials set ? ',insertData,function(err,rows) {            
+    if(err) {
+      console.log("Error Selecting : %s ",err );
+    }
+
+  });
+}
+
+module.exports = router;
