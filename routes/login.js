bc8f073feb939acb7d232340472547d743c2359a
Arjun93 <nujra.an@gmail.com>
2015-10-03 01:30:15 -0400

    code clean

--- routes/login.js
+++ routes/login.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  //host     : 'quizapp.ccwtwgtut47e.us-east-1.rds.amazonaws.com',
+  //host     : 'localhost',
+  host     : 'quizapp.ccwtwgtut47e.us-east-1.rds.amazonaws.com',
   port : '3306',
   user     : 'root',
   password : '12312312', 
