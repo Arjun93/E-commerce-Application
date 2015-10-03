ba83ec918a2a7d148d53a5303ac701a37ca80d46
Arjun93 <nujra.an@gmail.com>
2015-10-03 03:10:49 -0400

    Health test file created

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 var storeOptions = {
-    host: 'localhost',
-    //host: 'quizapp.ccwtwgtut47e.us-east-1.rds.amazonaws.com',
+    //host: 'localhost',
+    host: 'quizapp.ccwtwgtut47e.us-east-1.rds.amazonaws.com',
     port: 3306,
     user: 'root',
     password: '12312312',
